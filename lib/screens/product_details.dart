eb552105ac82a7cb7c160b9897dc420e86a658b3
RedBaron1918 <asatianikotiko1@gmail.com>
2023-02-16 03:25:24 -0800

    fixed overflow bug

--- lib/screens/product_details.dart
+++ lib/screens/product_details.dart
@@ -13,50 +13,52 @@ class ProductDetails extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Hero(
-              tag: details.productName,
-              child: Container(
-                height: 220,
-                width: 220,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                      width: 1,
-                      color: const Color.fromARGB(255, 231, 231, 231)),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Image.asset("assets/${details.img}"),
+        child:SingleChildScrollView(
+          child:  Column(
+        children: [
+          Hero(
+            tag: details.productName,
+            child: Container(
+              height: 220,
+              width: 220,
+              decoration: BoxDecoration(
+                border: Border.all(
+                    width: 1,
+                    color: const Color.fromARGB(255, 231, 231, 231)),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Image.asset("assets/${details.img}"),
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Price:${details.price}\$",
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 18.0,
               ),
             ),
-            const SizedBox(
-              height: 15,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "Price:${details.price}\$",
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 18.0,
-                ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              details.description,
+              textAlign: TextAlign.justify,
+              style: const TextStyle(
+                fontSize: 20.0,
               ),
             ),
-            const SizedBox(
-              height: 15,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                details.description,
-                textAlign: TextAlign.justify,
-                style: const TextStyle(
-                  fontSize: 20.0,
-                ),
-              ),
-            ),
-          ],
+          ),
+        ],
         ),
+        )
       ),
     );
   }
